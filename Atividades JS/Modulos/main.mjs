a9ceb89e6a3acfa9f0d3e9759878694fefca8859
agouveiabr <agouveialins@gmail.com>
2022-03-25 18:43:15 -0300

    Modulos - JS

---
+++ Atividades JS/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Alvaro", 23));
+console.log(mostraCidade("Alvaro", "RJ"));
+console.log(mostraHobby("Alvaro", "Tennis"));
