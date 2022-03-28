54ad70a7f5a89a034ac6caee6ea14bdf4e768232
agouveiabr <agouveialins@gmail.com>
2022-03-28 20:43:11 -0300

    Dino Game - Finalizado

--- Desafios-de-Projeto/Jogo-Dinossauro/script.js
+++ Desafios-de-Projeto/Jogo-Dinossauro/script.js
@@ -2,6 +2,8 @@ const dino = document.querySelector(".dino");
 let isJumping = false;
 const background = document.querySelector(".background");
 let position = 0;
+let score = 0;
+let gameOver = false;
 
 function handleKeyUp(e) {
   //? e.keyCode = tecla pressionada
@@ -37,12 +39,13 @@ function jump() {
 
 function createCactus() {
   const cactus = document.createElement("div");
-  let cactusPosition = 1000;
-  let randomTime = Math.random() * 6000;
+  let cactusPosition = 1800;
+  let randomTime = Math.floor(Math.random() * (2000 - 1000)) + 1000; //? numero entre 1k e 2k
+
   console.log(randomTime);
 
   cactus.classList.add("cactus");
-  cactus.style.left = 1000 + "px";
+  cactus.style.left = 1800 + "px";
   background.appendChild(cactus);
 
   let leftInterval = setInterval(() => {
@@ -50,16 +53,29 @@ function createCactus() {
       clearInterval(leftInterval);
       background.removeChild(cactus);
     } else if (cactusPosition > 0 && cactusPosition < 60 && position <= 60) {
+      gameOver = true;
       clearInterval(leftInterval);
       background.removeChild(cactus);
-      document.body.innerHTML = "<h1>GAME OVER</h1>";
-    } else {
+      document.body.innerHTML =
+        '<h1 class="game-over">GAME OVER</h1><h2 class="game-over">Sua pontuação foi: ' +
+        score +
+        "</h2>" +
+        '<button class="reset">Reiniciar</button>';
+      document.querySelector(".reset").addEventListener("click", resetGame);
+    } else if (!gameOver) {
+      console.log(gameOver);
+      score++;
+      document.querySelector(".score").innerHTML = "Score: " + score;
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + "px";
     }
   }, 20);
 
   setTimeout(createCactus, randomTime);
+}
+
+function resetGame() {
+  location.reload(); //? recarrega a pagina
 }
 
 createCactus();
